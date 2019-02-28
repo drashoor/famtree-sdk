2edf783993bcce80304a9e643e42e4ccbc8b507e
Yassir Awad <yassir.awad@dce.sa>
2019-02-28 13:01:42 +0200

    updates

---
+++ src/API/Facades/Famtree.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Digital 2
+ * Date: 2/28/2019
+ * Time: 12:58 PM
+ */
+
+namespace FamtreeV3\API\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Famtree extends Facade
+{
+    protected static function getFacadeAccessor() { return 'famtree'; }
+}
--- src/FamtreeServiceProvider.php
+++ src/FamtreeServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace FamtreeV3;
 
+use FamtreeV3\API\Famtree;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class FamtreeServiceProvider extends ServiceProvider
@@ -16,5 +18,12 @@ class FamtreeServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . './config/famtree.php' => config_path('famtree.php'),
         ], 'config');
+    }
+
+    public function register()
+    {
+        App::bind('famtree', function () {
+            return new Famtree();
+        });
     }
 }
