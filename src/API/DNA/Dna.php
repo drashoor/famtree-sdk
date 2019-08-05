22d22a3a8e0059565f29465ebce55b8f6c8481db
Yassir Awad <yassir.awad@dce.sa>
2019-08-05 13:55:46 +0300

    add change status and cancel of dna request

---
+++ src/API/DNA/Dna.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Digital 2
+ * Date: 2/18/2019
+ * Time: 12:27 PM
+ */
+
+namespace FamtreeV3\API\FamilyRequest;
+
+use FamtreeV3\API\Client;
+
+class Dna extends Client
+{
+    public function changeStatus(int $orderId, $data)
+    {
+        return self::patch("api/admin/dna-requests/$orderId", $data);
+    }
+
+    public function cancel(int $orderId, $data)
+    {
+        return self::patch("api/admin/dna-requests/$orderId/cancel", $data);
+    }
+}
